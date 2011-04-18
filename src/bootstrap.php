9234ae0ffad9bd75918c461a71ff4b2a385847b2
Joeri <joeriverdeyen@gmail.com>
2011-04-18 22:21:09 +0200

    Bootstrap fix

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -8,12 +8,12 @@ try{
   include_once dirname(__FILE__).'/../../config/db.php';
   include_once dirname(__FILE__).'/Autoloader.php';
   
-  Framework\Autoloader::getInstance()->registerNamespace('Framework',dirname(__FILE__));
-  Framework\Autoloader::getInstance()->registerNamespace(APP_NAME,ROOT_DIR.'./');
-  Framework\Autoloader::getInstance()->register();
+  Autoloader::getInstance()->registerNamespace('Framework',dirname(__FILE__));
+  Autoloader::getInstance()->registerNamespace(APP_NAME,ROOT_DIR.'./');
+  Autoloader::getInstance()->register();
   
-  Framework\Logger::getInstance()->setErrorHandlers();
-  echo Framework\FrontController::getInstance()->route();
+  Logger::getInstance()->setErrorHandlers();
+  echo FrontController::getInstance()->route();
 
 }catch(\Exception $e){
   //TODO check of er een error controller bestaat, anders standaard error tonen
